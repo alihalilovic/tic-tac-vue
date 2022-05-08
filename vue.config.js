eb6949c893c9a487f56d80ed39cc7f3f27d3e3cc
alihalilovic <halilovicali03@gmail.com>
2022-05-08 18:43:09 +0200

    updated config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-tic-tac-toe/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/tic-tac-vue/" : "/",
 };
